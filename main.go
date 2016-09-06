1dfbad3601a9aaf7e1be34fd25079833a09c8605
Vitaly Lapkovsky <v.lapkovsky@viber.com>
2016-09-06 19:24:50 +0300

    simple example

--- main.go
+++ main.go
@@ -1,13 +1,37 @@
 package main
 
-import "net/http"
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 func handler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Hi there, I love %s!", request.URL.Path[1:])
+	// image, error := ioutil.ReadFile("/Users/vitaly.lapkovsky/Documents/test.png")
+	// check(error)
+	query := request.URL.Query()
+	imageUrl := query.Get("imageUrl")
+
+	if len(imageUrl) == 0 {
+		writer.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(writer, "Bad url request: %s", request.URL)
+		return
+	}
+
+	fmt.Fprint(writer, imageUrl)
+
+	// writer.Header().Set("Content-Lenght", strconv.Itoa(len(image)))
+	// writer.WriteHeader(http.StatusOK)
+	// writer.Write(image)
+	// fmt.Fprintf(writer, "Hi there, I love %s!", request.URL.Path[1:])
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/resize", handler)
 	http.ListenAndServe(":8080", nil)
 }
